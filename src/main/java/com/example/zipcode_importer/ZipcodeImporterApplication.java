d6d4b9677c0bdb100b2650e46cce0955efabd2e6
Josh <jpiff57@gmail.com>
2024-09-19 12:52:35 -0700

    Initial Project Commit

---
+++ src/main/java/com/example/zipcode_importer/ZipcodeImporterApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipcode_importer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipcodeImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipcodeImporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zipcode_importer/ZipcodeImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zipcode_importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipcodeImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
